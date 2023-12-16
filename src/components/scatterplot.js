45b3058dd081096ac47a56ad52d5b771a625b381
iridiox <tyler.jay.c@gmail.com>
2023-12-15 18:16:45 -0800

    added colors to scatterplot

--- src/components/scatterplot.js
+++ src/components/scatterplot.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import {
-  Chart as ChartJS,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Tooltip,
-  Legend,
-} from 'chart.js';
+import { Chart as ChartJS, LinearScale, PointElement, LineElement, Tooltip, Legend } from 'chart.js';
 import { Scatter } from 'react-chartjs-2';
 
-
-function Scatterplot({covidData}) {
-
-  // debug statement
-  // console.log("chart Data: ", covidData);
-
+function Scatterplot({ covidData }) {
   ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
 
-   const options = {
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const options = {
     scales: {
       y: {
         beginAtZero: true,
@@ -44,17 +41,17 @@ function Scatterplot({covidData}) {
           y: covidData.deaths,
         },
       ],
-      backgroundColor: 'rgba(255, 30, 100, 1)',
-
+      backgroundColor: getRandomColor(),
     })),
   };
 
-    return (
-      <div>
+  return (
+    <div>
       <h3>Cases and Deaths per Continent</h3>
-    <Scatter options={options} data={data} />
-      </div>
-    )
+      <Scatter options={options} data={data} />
+    </div>
+  );
 }
 
 export default Scatterplot;
+
